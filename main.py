7dd4efa60d6f5a14171f5217f7b7dad9a5a2b700
Caden Tebow <cadentebow@Cadens-MacBook-Air.local>
2025-08-16 18:01:20 -0500

    move main up one file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from src.classes import run_algo
+from backend.src.classes import run_algo
 import json
 
 app = FastAPI()
